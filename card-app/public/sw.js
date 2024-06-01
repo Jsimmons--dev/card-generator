d8bca553d24714006064b9e352d7cfdc0c425702
Joshua Simmons <josh@phinsecurity.com>
2024-06-01 14:28:29 -0400

    Fixed sw for dev at least

--- card-app/public/sw.js
+++ card-app/public/sw.js
@@ -2,20 +2,13 @@ import { precacheAndRoute } from 'workbox-precaching'
 
 precacheAndRoute(self.__WB_MANIFEST)
 
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here. Other Firebase libraries
-// are not available in the service worker.
-// importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
-
 import { initializeApp } from "firebase/app";
 import { onBackgroundMessage } from "firebase/messaging/sw";
 import { getMessaging } from "firebase/messaging/sw";
+import { onMessage } from 'firebase/messaging';
 
+console.log('initializing firebase app');
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
 initializeApp({
   apiKey: "AIzaSyB20se1or3zx5FfELCpx6BG1lVZGzI8Mjs",
   authDomain: "cards-425002.firebaseapp.com",
@@ -26,29 +19,24 @@ initializeApp({
   measurementId: "G-9NHNQPXDH5"
 });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-// const messaging = firebase.messaging();
 const messaging = getMessaging();
 
-// messaging.onBackgroundMessage((payload) => {
-//   console.log(
-//     '[firebase-messaging-sw.js] Received background message ',
-//     payload
-//   );
+
+const notificationTitle = 'Background Message Title';
+const notificationOptions = {
+  body: 'Background Message body.',
+  icon: '/vite.svg'
+};
 
 onBackgroundMessage(messaging, (payload) => {
   console.log(
     '[firebase-messaging-sw.js] Received background message ',
     payload
   );
-
-  // Customize notification here
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/logo.png'
-  };
-
   self.registration.showNotification(notificationTitle, notificationOptions);
+})
+
+onMessage(messaging, (payload) => {
+  console.log('Message received. ', payload);
 });
+
--- card-app/src/main.jsx
+++ card-app/src/main.jsx
@@ -4,15 +4,34 @@ import App from './App.jsx'
 import './index.css'
 import { getAnalytics } from "firebase/analytics";
 import { app } from './firebaseConfig.js'
-import { getMessaging, onMessage } from "firebase/messaging";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 
 import { store } from './store'
 import { Provider } from 'react-redux';
+import { onBackgroundMessage } from 'firebase/messaging/sw';
 
 const analytics = getAnalytics(app)
 
 const messaging = getMessaging();
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register(    
+      import.meta.env.MODE === 'production' ? '/sw.js' : '/dev-sw.js?dev-sw',
+      { type: import.meta.env.MODE === 'production' ? 'classic' : 'module' }
+    )
+    .then((registration) => {
+      getToken(messaging, {
+          vapidKey: 'BOn9hE0IqDaTwMj7mUFhP1K7cD4HJFudbBAAQSIz_MJOrawRmbgvobvsldTBoO21_ZCoHEMVcjkGCnwei3gi8Ow',
+          serviceWorkerRegistration : registration 
+      })
+        .then((currentToken) => {
+          // do something
+          console.log(currentToken)
+        });
+     });
+}
 onMessage(messaging, (payload) => {
   console.log('Message received. ', payload);
 });
