5effac22e70c222f920a8df5b8166aded051a3ca
Joshua Simmons <josh@phinsecurity.com>
2024-06-01 12:59:14 -0400

    try again

--- card-app/src/firebase-messaging-sw.js
+++ card-app/src/firebase-messaging-sw.js
@@ -1,8 +1,16 @@
-import { getMessaging } from "firebase/messaging/sw";
-import { onBackgroundMessage } from "firebase/messaging/sw";
+importScripts("https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js");
+const swScriptUrl = new URL(self.location);
 
-const messaging = getMessaging();
-onBackgroundMessage(messaging, (payload) => {
+const base64Config = swScriptUrl.searchParams.get('firebaseConfig')
+let decoded = atob(base64Config);
+
+const firebaseConfig = JSON.parse(decoded);
+const app = firebase.initializeApp(firebaseConfig);
+
+
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
   const notificationTitle = 'Background Message Title';
