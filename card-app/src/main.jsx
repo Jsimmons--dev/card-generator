d7c9510477e269abe75e895bf2e2d2dc47ed82e5
Joshua Simmons <josh@phinsecurity.com>
2024-06-01 12:50:43 -0400

    Do some service worker stuff to see if it works

--- card-app/src/main.jsx
+++ card-app/src/main.jsx
@@ -4,12 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import { getAnalytics } from "firebase/analytics";
 import { app } from './firebaseConfig.js'
+import { getMessaging, onMessage } from "firebase/messaging";
 
 
 import { store } from './store'
 import { Provider } from 'react-redux';
 
 const analytics = getAnalytics(app)
+
+const messaging = getMessaging();
+onMessage(messaging, (payload) => {
+  console.log('Message received. ', payload);
+});
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- card-app/vite.config.js
+++ card-app/vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      plugins: [react()],
+    };
+  } else {
+    // command === 'build'
+    return {
+      plugins: [react()],
+      build: {
+        target: "es2022",
+        rollupOptions: {
+          input: {
+            "main": "./index.html",
+            "firebase-messaging-sw": "./src/firebase-messaging-sw.js",
+          },
+          output: {
+            entryFileNames: (chunkInfo) => {
+              return chunkInfo.name === "firebase-messaging-sw"
+                ? "[name].js" // Output service worker in root
+                : "assets/[name]-[hash].js"; // Others in `assets/`
+            },
+          },
+        },
+      },
+    };
+  }
+});
