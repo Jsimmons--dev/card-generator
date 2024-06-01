ea0477c19d9dbbf9be5d9f24aebe0f3816007c9e
Joshua Simmons <josh@phinsecurity.com>
2024-06-01 13:57:25 -0400

    trying again with service worker

--- card-app/public/sw.js
+++ card-app/public/sw.js
@@ -5,14 +5,18 @@ precacheAndRoute(self.__WB_MANIFEST)
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here. Other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
+import { initializeApp } from "firebase/app";
+import { onBackgroundMessage } from "firebase/messaging/sw";
+import { getMessaging } from "firebase/messaging/sw";
 
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-firebase.initializeApp({
+initializeApp({
   apiKey: "AIzaSyB20se1or3zx5FfELCpx6BG1lVZGzI8Mjs",
   authDomain: "cards-425002.firebaseapp.com",
   projectId: "cards-425002",
@@ -24,13 +28,21 @@ firebase.initializeApp({
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
+const messaging = getMessaging();
 
-messaging.onBackgroundMessage((payload) => {
+// messaging.onBackgroundMessage((payload) => {
+//   console.log(
+//     '[firebase-messaging-sw.js] Received background message ',
+//     payload
+//   );
+
+onBackgroundMessage(messaging, (payload) => {
   console.log(
     '[firebase-messaging-sw.js] Received background message ',
     payload
   );
+
   // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
@@ -39,4 +51,4 @@ messaging.onBackgroundMessage((payload) => {
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
-});k
+});
--- card-app/vite.config.js
+++ card-app/vite.config.js
@@ -8,40 +8,23 @@ export default defineConfig({
     react(),
     VitePWA({
       mode: 'development',
+      devOptions: {
+        enabled: true,
+        type:'module'
+      },
       injectManifest: {
         globPatterns: ['**/*'],
       },
       strategies: 'injectManifest',
       srcDir: 'public',
-      filename: 'firebase-messaging-sw.mjs',
+      filename: 'sw.js',
       manifest: {
-        name: 'D-RSS',
-        short_name: 'D-RSS',
+        name: 'Cards',
+        short_name: 'Cards',
         theme_color: '#06091c',
         display: 'standalone',
         background_color: '#06091c',
         icons: [
-          {
-            src: 'logo-48-48.png',
-            sizes: '48x48',
-            type: 'image/png',
-          },
-          {
-            src: 'logo-96-96.png',
-            sizes: '96x96',
-            type: 'image/png',
-          },
-          {
-            src: 'logo-512-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'logo-512-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
         ],
       },
     })
