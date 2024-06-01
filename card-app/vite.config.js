4621585b56898c09a3e58525e084d14a285b8333
Joshua Simmons <josh@phinsecurity.com>
2024-06-01 13:17:32 -0400

    Change plugin back to react for vite config

--- card-app/vite.config.js
+++ card-app/vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   base: '',
   plugins: [
-    vue(),
+    react(),
     VitePWA({
       mode: 'development',
       injectManifest: {
